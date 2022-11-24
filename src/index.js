741809ead299285fa789d0e803647a5f1b0060f3
Piotr Filipiak <kiddhkane@gmail.com>
2022-11-24 18:47:38 +0100

    Added edit fields.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import { TextControl } from "@wordpress/components";
 import "./index.scss";
+import {
+  TextControl,
+  Flex,
+  FlexBlock,
+  FlexItem,
+  Button,
+  Icon,
+} from "@wordpress/components";
 
 wp.blocks.registerBlockType("quizplugin/quizblock", {
   title: "Quiz Block",
@@ -25,9 +32,25 @@ function EditComponent(props) {
   }
 
   // TextControl is a WordPress element
+  // FlexBlock takes all the space it can
+  // FlexItem only takes as much as it needs
   return (
     <div className="quizEditBlock">
       <TextControl label="Question:" />
+      <p>Answers:</p>
+      <Flex>
+        <FlexBlock>
+          <TextControl />
+        </FlexBlock>
+        <FlexItem>
+          <Button>
+            <Icon icon="star-empty" />
+          </Button>
+        </FlexItem>
+        <FlexItem>
+          <Button>Delete</Button>
+        </FlexItem>
+      </Flex>
     </div>
   );
 }
