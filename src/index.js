88d023ac7b984066464a00c19f1e925293c2905b
Piotr Filipiak <kiddhkane@gmail.com>
2022-11-24 21:51:04 +0100

    Displaying all the answers.

--- src/index.js
+++ src/index.js
@@ -13,8 +13,9 @@ wp.blocks.registerBlockType("quizplugin/quizblock", {
   icon: "smiley",
   category: "common",
   attributes: {
-    skyColor: { type: "string" },
-    grassColor: { type: "string" },
+    question: { type: "string" },
+    answers: { type: "array", default: ["red", "blue"] },
+    correctAnswer: { type: "number", default: undefined },
   },
   edit: EditComponent,
   save: function () {
@@ -23,12 +24,16 @@ wp.blocks.registerBlockType("quizplugin/quizblock", {
 });
 
 function EditComponent(props) {
-  function updateSkyColor(e) {
-    props.setAttributes({ skyColor: e.target.value });
+  function updateQuestion(value) {
+    props.setAttributes({ question: value });
   }
 
-  function updateGrassColor(e) {
-    props.setAttributes({ grassColor: e.target.value });
+  function deleteAnswer(indexToDelete) {
+    // console.log("Hey");
+    const newAnswers = props.attributes.answers.filter((x, index) => {
+      return index != indexToDelete;
+    });
+    props.setAttributes({ answers: newAnswers });
   }
 
   // TextControl is a WordPress element
@@ -36,24 +41,56 @@ function EditComponent(props) {
   // FlexItem only takes as much as it needs
   return (
     <div className="quizEditBlock">
-      <TextControl label="Question:" style={{ fontSize: "20px" }} />
+      <TextControl
+        label="Question:"
+        value={props.attributes.question}
+        onChange={updateQuestion}
+        style={{ fontSize: "20px" }}
+      />
       <p style={{ fontSize: 13, margin: "20px 0 8px" }}>Answers:</p>
-      <Flex>
-        <FlexBlock>
-          <TextControl />
-        </FlexBlock>
-        <FlexItem>
-          <Button>
-            <Icon className="markAsCorrect" icon="star-empty" />
-          </Button>
-        </FlexItem>
-        <FlexItem>
-          <Button isLink className="delete">
-            Delete
-          </Button>
-        </FlexItem>
-      </Flex>
-      <Button variant="primary">Add another answer</Button>
+
+      {props.attributes.answers.map(function (answer, index) {
+        return (
+          <Flex>
+            <FlexBlock>
+              <TextControl
+                value={answer}
+                onChange={(newValue) => {
+                  const newAnswers = props.attributes.answers.concat([]);
+                  newAnswers[index] = newValue;
+                  props.setAttributes({ answers: newAnswers });
+                }}
+                autoFocus={answer == undefined}
+              />
+            </FlexBlock>
+            <FlexItem>
+              <Button>
+                <Icon className="markAsCorrect" icon="star-empty" />
+              </Button>
+            </FlexItem>
+            <FlexItem>
+              <Button
+                isLink
+                className="delete"
+                onClick={() => deleteAnswer(index)}
+              >
+                Delete
+              </Button>
+            </FlexItem>
+          </Flex>
+        );
+      })}
+
+      <Button
+        variant="primary"
+        onClick={() => {
+          props.setAttributes({
+            answers: props.attributes.answers.concat([undefined]),
+          });
+        }}
+      >
+        Add another answer
+      </Button>
     </div>
   );
 }
