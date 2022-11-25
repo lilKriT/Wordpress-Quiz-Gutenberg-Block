4f587180846be74e8ec1d889b1ee4e811bb73892
Piotr Filipiak <kiddhkane@gmail.com>
2022-11-25 18:00:04 +0100

    Displaying questions and answers.

--- index.php
+++ index.php
@@ -41,7 +41,9 @@ class QuizBlock
         }
 
         ob_start(); ?>
-        <div class="paying-attention-update-me"></div>
+        <div class="paying-attention-update-me">
+            <pre style="display: none;"><?php echo wp_json_encode($attributes); ?></pre>
+        </div>
 <?php return ob_get_clean();
     }
 }
